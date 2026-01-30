40959e92a99eda81c5493f57f0df9fa89a4c3806
LagunasRafael <134352272+LagunasRafael@users.noreply.github.com>
2026-01-30 15:37:33 -0600

    feat(auth): registro y login funcionando

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from app.auth.jwt import create_access_token
 
 from app.auth import schemas, service
 from app.db.database import get_db
@@ -9,3 +10,13 @@ router = APIRouter()
 @router.post("/register", response_model=schemas.UserResponse)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return service.create_user(db, user)
+
+@router.post("/login", response_model=schemas.Token)
+def login(data: schemas.UserLogin, db: Session = Depends(get_db)):
+    user = service.authenticate_user(db, data.email, data.password)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"sub": str(user.id)})
+    return {"access_token": token}
+
--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.auth import models, schemas
-from app.auth.segurity import hash_password
+from app.auth.segurity import hash_password, verify_password
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
@@ -12,5 +12,13 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
 
 ##
