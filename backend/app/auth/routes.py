97f60eede5f7acb88ee6e708972987f142fb5303
LagunasRafael <134352272+LagunasRafael@users.noreply.github.com>
2026-01-30 14:16:24 -0600

    commitregister

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.auth import schemas, service
+from app.db.database import get_db
 
 router = APIRouter()
 
-@router.get("/ping")
-def auth_ping():
-    return {"module": "auth", "status": "ok"}
+@router.post("/register", response_model=schemas.UserResponse)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return service.create_user(db, user)
---
+++ backend/app/auth/service.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from app.auth import models, schemas
+from app.auth.segurity import hash_password
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(
+        email=user.email,
+        hashed_password=hash_password(user.password),
+        full_name=user.full_name
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
