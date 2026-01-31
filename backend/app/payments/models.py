b113e87b6d9328d492d4944c7bda012b73362db0
Gabriel <diegogabrielmartinezalanis@gmail.com>
2026-01-30 19:04:25 -0600

    feat(payments): Modelo de base de datos inicial

---
+++ backend/app/payments/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from app.db.database import Base
+import enum
+
+class PaymentStatus(str, enum.Enum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    FAILED = "failed"
+    REFUNDED = "refunded"
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float, nullable=False)
+    status = Column(String, default=PaymentStatus.PENDING)
+    payment_method = Column(String, default="card")
+    provider_payment_id = Column(String, nullable=True) #ID del proveedor de pago
+    
+    # Relaciones (Foreign Keys)
+    payer_id = Column(Integer, ForeignKey("users.id")) 
+    payee_id = Column(Integer, ForeignKey("users.id"))
+    service_id = Column(Integer, ForeignKey("services.id"))
+    
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
